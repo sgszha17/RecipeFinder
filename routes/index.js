76bae19e0f3dc80f42c144654580c58c9c45c237
Siyu Zhang <paula555@outlook.com>
2018-07-01 00:57:00 +1000

    Get data from the user

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title:"express"});
+  // console.log("ok")
 });
+
+router.get('/upload_data',function (req,res,next) {
+    // res.send('hello')
+    console.log("got request")
+})
+
 
 module.exports = router;
